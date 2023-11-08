58380a6ce137fca36ba40202d90cc7adbafc0dbc
stilyan91 <stilyan91@gmail.com>
2023-11-08 09:46:58 +0200

    init repo

---
+++ src/components/UserList.jsx
@@ -0,0 +1,17 @@
+import Search from './Search';
+import UserListTable from './UserListTable';
+const UserList = () => {
+    return (
+        <section className="card users-container">
+
+            <Search />
+
+            <UserListTable/>
+
+        </section>
+
+
+    );
+};
+
+export default UserList;
